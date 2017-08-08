6a74ffd4e76ce2179660538a5cc2d72014ef9916
alf <alf@akademe.net>
2017-08-07 16:18:37 -1000

    able to manifest books. sorted but unsorted in DOM

---
+++ src/ListBooks.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+// import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
+
+class ListBooks extends Component {
+	static propTypes = {
+		books: PropTypes.array,
+	}
+
+	state = {
+		query: ''
+	}
+
+	updateQuery = (query) => {
+		this.setState({ query: query.trim() })
+	}
+
+	clearQuery = () => {
+		this.setState({ query: ''})
+	}
+
+	render () {
+		const { books } = this.props
+		const { query } = this.state
+
+		let showingBooks
+		if (query) {
+			const match = new RegExp(escapeRegExp(query), 'i')
+			showingBooks = books.filter((book) => match.test(book.title))
+		} else {
+			showingBooks = books
+		}
+
+		showingBooks.sort(sortBy('shelf'))
+
+		return (
+			<ol className="books-grid">
+			{console.log({showingBooks})}
+			{showingBooks.map((book) => (
+				<li key={book.id}>
+					<div className="book">
+						<div className="book-top" style={{ width: 128, height: 193,
+						backgroundImage: `url(${book.imageLinks.thumbnail})`}}>
+
+						</div>
+						<div className="book-title">{book.title}</div>
+						<div className="book-authors">{book.authors}</div>
+
+					</div>
+				</li>
+			))}
+			</ol>
+		)
+
+	}
+}
+
+export default ListBooks
